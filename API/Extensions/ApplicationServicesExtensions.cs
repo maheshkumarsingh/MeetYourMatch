97a3889bb6a29e525abd0bd63849d99ca6bbe85c
maheshkumarsingh <sm.singhmahesh@gmail.com>
2025-06-30 00:07:19 +0530

    production ready code

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -14,7 +14,7 @@ public static class ApplicationServicesExtensions
     {
         services.AddDbContext<DataContext>(opt =>
         {
-            opt.UseSqlServer(configuration.GetConnectionString("ProdConnection"));
+            opt.UseSqlServer(configuration.GetConnectionString("DBConnection"));
         });
         services.AddHttpClient();
         services.AddApiVersioning(options =>
